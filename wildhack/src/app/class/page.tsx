ab27a6b01123920761e199b568c3b75e4fe91abc
elainecui9 <elaine.cui@gmail.com>
2024-04-07 11:27:38 -0500

    hi

--- wildhack/src/app/class/page.tsx
+++ wildhack/src/app/class/page.tsx
@@ -4,7 +4,7 @@ import Footer from "../footer";
 import Dashboard from "./classdash";
 import {useState, useEffect} from "react";
 
-  const students = ["David Wu", "Chirs Heo", "Elaine Cui", "John Adams"]
+const students = ["David Wu", "Chirs Heo", "Elaine Cui", "John Adams"]
 
 export default function ClassDashboard() {
   const [articles, setarticles] = useState<
@@ -26,25 +26,12 @@ const [folders, setfolders] = useState<
   }];
 }[]
 >([]);
-async function getData(){
-    const res = await fetch("http://localhost:4000/class6612bb18f35d55159e1a982a",{
-        method:"GET",
-        credentials: 'include',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-    })
-    const data = await res.json();
-    setarticles(data.articles);
-    setfolders(data.folders);
-}
-
 const [path, setPath] = useState<string>("Home > ");
 const [infolder, setinfolder] = useState<boolean>(false);
 const [rerender, setrerender] = useState<boolean>(false);
 const [students, setStudents]= useState();
 async function getData(){
-    const res = await fetch("http://localhost:4000/class/send", {
+    const res = await fetch("http://localhost:4000/class/6612bb18f35d55159e1a982a", {
         method: "GET",
         credentials: 'include',
         headers: {
@@ -59,9 +46,7 @@ async function getData(){
 }
 
 useEffect(() => {
-
     getData();
-
   setrerender(false);
   setinfolder(false);
   setPath("Home > ");
@@ -73,7 +58,7 @@ useEffect(() => {
   return (
     <section className="bg-white">
       <Header></Header>
-    <Dashboard students = {students} name="Chris" setrerender = {setrerender} infolder= {infolder} setinfolder = {setinfolder} path = {path} setPath = {setPath} folders = {folders} setfolders = {setfolders} setarticles = {setarticles} articles = {articles}></Dashboard>
+    <Dashboard students = {students} name="" setrerender = {setrerender} infolder= {infolder} setinfolder = {setinfolder} path = {path} setPath = {setPath} folders = {folders} setfolders = {setfolders} setarticles = {setarticles} articles = {articles}></Dashboard>
 
       <Footer></Footer>
     </section>
