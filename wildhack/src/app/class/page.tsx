a37c3ab8444d6b83c730d3cb95eebd6183319841
elainecui9 <elaine.cui@gmail.com>
2024-04-07 11:03:03 -0500

    class

--- wildhack/src/app/class/page.tsx
+++ wildhack/src/app/class/page.tsx
@@ -1,97 +1,9 @@
 "use client"
 import Header from "../frontpage/header";
 import Footer from "../footer";
-import Folders from "./folders";
 import Dashboard from "./classdash";
 import {useState, useEffect} from "react";
 
-
-const fold = [
-  {
-    name: 'CS',
-    datecreated: '2023-01-23',
-    href:"#",
-    articles: [{
-        name: 'B1',
-        datecreated: '2023-01-23',
-        href:"#",
-      },
-      {
-        name: 'B2',
-        datecreated: '2023-01-23',
-        href:"#",
-      },
-      {
-        name: 'B3',
-        datecreated: '2023-01-23',
-        href:"#",
-      }
-    ]
-  },
-  {
-    name: 'Social',
-    datecreated: '2023-01-23',
-    href:"#",
-    articles: [{
-      name: 'H1',
-      datecreated: '2023-01-23',
-      href:"#",
-    },
-    {
-      name: 'H2',
-      datecreated: '2023-01-23',
-      href:"#",
-    },
-    {
-      name: 'H3',
-      datecreated: '2023-01-23',
-      href:"#",
-    }
-  ]
-  },
-  {
-    name: 'CS 150',
-    datecreated: '2023-01-23',
-    href:"#",
-    articles: [{
-      name: 'CS1',
-      datecreated: '2023-01-23',
-      href:"#",
-    },
-    {
-      name: 'CS2',
-      datecreated: '2023-01-23',
-      href:"#",
-    },
-    {
-      name: 'CS3',
-      datecreated: '2023-01-23',
-      href:"#",
-    }
-  ]
-  },
-
-]
-
-
-const art = [
-    {
-      name: 'What is life',
-      datecreated: '2023-01-23',
-      href:"#",
-    },
-    {
-      name: 'Psychology of CS',
-      datecreated: '2023-01-23',
-      href:"#",
-    },
-    {
-      name: 'Does water expire',
-      datecreated: '2023-01-23',
-      href:"#",
-    },
-   
-  ]
   const students = ["David Wu", "Chirs Heo", "Elaine Cui", "John Adams"]
 
 export default function ClassDashboard() {
@@ -114,14 +26,25 @@ const [folders, setfolders] = useState<
   }];
 }[]
 >([]);
+async function getData(){
+    const res = await fetch("http://localhost:4000/class6612bb18f35d55159e1a982a",{
+        method:"GET",
+        credentials: 'include',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+    })
+    const data = await res.json();
+    setarticles(data.articles);
+    setfolders(data.folders);
+}
 
 const [path, setPath] = useState<string>("Home > ");
 const [infolder, setinfolder] = useState<boolean>(false);
 const [rerender, setrerender] = useState<boolean>(false);
 
 useEffect(() => {
-  setarticles(art);
-  setfolders(fold);
+    getData();
   setrerender(false);
   setinfolder(false);
   setPath("Home > ");
