069673254c2e66192f5d8a2344c50d20f9c86053
elainecui9 <elaine.cui@gmail.com>
2024-04-07 11:10:22 -0500

    ui

--- wildhack/src/app/userdashboard/articles.tsx
+++ wildhack/src/app/userdashboard/articles.tsx
@@ -3,26 +3,30 @@ import { MdArticle } from "react-icons/md";
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
 import Editbuttonarticle from "./editbuttonarticle";
 
-export default function Articles({article, key})
-{
+export default function Articles({ article }) {
+    const formattedDate = new Date(article.date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
 
     return (
         <section className="flex justify-between items-center gap-x-6 py-5">
-        <div className="flex gap-x-4 items-center">
-            <MdArticle style={{ color: 'black', width: '32px', height: '32px' }} />
-            <p className="text-md font-semibold leading-6 text-gray-900">
-                <a href={article.href}>
-                    {article.title}
-                </a>
-            </p>
-        </div>
-        <div className="flex-grow text-center">
-            <h1 className="text-sm text-gray-500">{article.date}</h1>
-        </div>
-        <div className="flex items-center gap-x-4">
-            <Editbuttonarticle></Editbuttonarticle>
-            <ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-        </div>
-    </section>
-    )
+            <div className="flex gap-x-4 items-center hover:cursor-pointer">
+                <MdArticle style={{ color: 'black', width: '32px', height: '32px' }} />
+                <p className="text-md font-semibold leading-6 text-gray-900">
+                    <a href={article.href}>
+                        {article.title}
+                    </a>
+                </p>
+            </div>
+            <div className="flex-grow text-center">
+                <h1 className="text-sm text-gray-500">{formattedDate}</h1>
+            </div>
+            <div className="flex items-center gap-x-4">
+                <Editbuttonarticle></Editbuttonarticle>
+                <ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+            </div>
+        </section>
+    );
 }
