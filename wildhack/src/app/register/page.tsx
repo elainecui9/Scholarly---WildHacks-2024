a4dee4a146db727f77b1cfdd30deae29449ba642
heochristopher <71462915+heochristopher@users.noreply.github.com>
2024-04-07 13:55:51 -0500

    .

--- wildhack/src/app/register/page.tsx
+++ wildhack/src/app/register/page.tsx
@@ -35,11 +35,6 @@ export default async function Register() {
         */}
         <div className="flex min-h-full h-screen bg-white flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=red&shade=900"
-              alt="Your Company"
-            />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-black">
               Register For An Account
             </h2>
