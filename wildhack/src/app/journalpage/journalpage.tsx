67ada9527b5b4eab9087574bf2488b220e5fd698
elainecui9 <elaine.cui@gmail.com>
2024-04-06 23:54:35 -0500

    editjournal

--- wildhack/src/app/journalpage/journalpage.tsx
+++ wildhack/src/app/journalpage/journalpage.tsx
@@ -4,19 +4,22 @@ import Back from "../components/back";
 export default function Journal() {
 
   return (
-    <section className="px-32 pt-8 flex justify-between">
+    <section className="p-8">
       <Back backpage="/journals"></Back>
-        <div >
-            <h1 className="font-bold text-3xl pb-2">JOURNAL TITLE</h1>
-            <h1 className="italic text-lg pb-8">Journal Author</h1>
-            <h1>Generated Text.......</h1>
-        </div>
-        <div>
-            <button className="text-white bg-black rounded-full px-4 py-2">
-                See Actual Journal
-            </button>
-        </div>
+        <section className="pt-8 pl-32 flex justify-between px-32">
+            <div >
+                <h1 className="font-bold text-3xl pb-2">JOURNAL TITLE</h1>
+                <h1 className="italic text-lg pb-8">Journal Author</h1>
+                <h1>Generated Text.......</h1>
+            </div>
+            <div>
+                <button className="text-white bg-black rounded-full px-4 py-2">
+                    See Actual Journal
+                </button>
+            </div>
+        </section>
     </section>
+    
     
   )
 }
