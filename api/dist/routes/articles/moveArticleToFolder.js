854e6e1b12f1fa24b2c2797a8d3115b55a5c6616
heochristopher <71462915+heochristopher@users.noreply.github.com>
2024-04-07 12:20:59 -0500

    delete article when moved to folder

--- api/dist/routes/articles/moveArticleToFolder.js
+++ api/dist/routes/articles/moveArticleToFolder.js
@@ -11,9 +11,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.moveArticleToFolder = void 0;
 const Folder_1 = require("../../models/Folder");
+const User_1 = require("../../models/User");
+const Class_1 = require("../../models/Class");
 const moveArticleToFolder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield Folder_1.Folder.findByIdAndUpdate(req.body.folder, { $push: { articles: req.body.article } });
+        if (req.body.user) {
+            yield User_1.User.findByIdAndUpdate(req.body.user, { $pull: { articles: req.body.article } });
+        }
+        else if (req.body.class) {
+            yield Class_1.Class.findByIdAndUpdate(req.body.class, { $pull: { articles: req.body.article } });
+        }
         res.status(200).send("Successfully moved article");
     }
     catch (error) {
